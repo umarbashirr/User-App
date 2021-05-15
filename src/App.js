649d88af3519889e3b167f80905a837b9350e156
Umar Bashir Rather <umarbashir93@outlook.com>
2021-05-15 16:47:51 +0530

    App Finalized

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import React, { useState } from 'react';
+import AddUser from './components/Users/AddUser';
+import UserList from './components/Users/UserList';
+
 function App() {
-  return <div className='App'>Hello World!</div>;
+  const [users, setUser] = useState([]);
+  const getUserData = (user) => {
+    setUser((prevUser) => {
+      return [user, ...prevUser];
+    });
+  };
+  return (
+    <div>
+      <AddUser onAddUser={getUserData} />
+      <UserList users={users} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Users/UserList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import classes from './UserList.module.css';
+import Card from '../UI/Card';
+
+const UserList = (props) => {
+  return (
+    <Card className={classes.users}>
+      <ul>
+        {props.users.map((user) => {
+          return (
+            <li key={user.id}>{`${user.username} (${user.age} Year Old)`}</li>
+          );
+        })}
+      </ul>
+    </Card>
+  );
+};
+
+export default UserList;
